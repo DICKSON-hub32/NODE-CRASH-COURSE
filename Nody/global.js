bdcb01377d850465dcfd7a4d4ca26e585a187985
Dickson Esamai <mukweesamai@gmail.com>
2025-03-16 18:02:20 +0300

    Multiple files added

---
+++ Nody/global.js
@@ -0,0 +1,14 @@
+//GLobal Object
+// console.log(global);
+
+// global.setTimeout(() => {
+//     console.log('In the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval (() => {
+//     console.log('In the Interval');
+// }, 1000); 
+
+// console.log(__dirname);
+// console.log(__filename);
